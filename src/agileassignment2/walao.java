a8615b9c38016d84b61cc648d4adf152c4e80e82
User <User@Tsukiyomi>
2018-12-11 01:52:45 +0800

    [#161880625] - add-sales-order-each-catalog

---
+++ src/agileassignment2/walao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agileassignment2;
+
+/**
+ *
+ * @author User
+ */
+public class walao {
+    public static void main(String[] args){
+        System.out.print("No more error pls");
+    }
+}
